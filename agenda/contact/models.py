d0b7b01594a51e70dee3e837f2d599b11f69d2b9
Sarah Soares <contatodev.sarah@gmail.com>
2023-11-26 22:46:31 -0300

    Cria modelo Contact

--- agenda/contact/models.py
+++ agenda/contact/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Contact(models.Model):
+    # id (primary_key, criado automaticamente)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    phone = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    # category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    # show = models.BooleanField(default=True)
+    # owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    # picture 
+
+    def __str__(self):
+        return self.frist_name + ' ' + self.last_name
+
