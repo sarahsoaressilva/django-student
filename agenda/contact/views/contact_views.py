e1734ca415ca81f6ecfa6d0562ba451efeba6566
Sarah Soares <contatodev.sarah@gmail.com>
2024-03-21 18:28:21 -0300

    [Develop] Contacts page

--- agenda/contact/views/contact_views.py
+++ agenda/contact/views/contact_views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from contact.models import *
 
 # Create your views here.
 def index(request):
+    contacts = Contact.objects.all()
+
+    context = {
+        'contacts': contacts
+    }
+
     return render(
         request, 
-        'contact/index.html'
+        'contact/index.html',
+        context
     )
